97287f350775e87966f3599b60de2726decb8507
Ekaterina Merkulova <kate_merek@mail.ru>
2025-05-05 09:49:36 +0300

    GeometryLib 1.1.0 добавила конструктор для квадрата

--- src/main/java/com/github/katemerek/geometrylib/Square.java
+++ src/main/java/com/github/katemerek/geometrylib/Square.java
@@ -2,6 +2,11 @@ package com.github.katemerek.geometrylib;
 
 public class Square implements Figure{
     int a;
+
+    public Square(int a) {
+        this.a = a;
+    }
+
     @Override
     public double getArea() {
         return a * a;
